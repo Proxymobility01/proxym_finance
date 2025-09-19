c4159b33f42d68756c2f9c40200b5f9dd30e6aef
MBY EKONDE Christian Durand <christiandior512@gmail.com>
2025-09-19 16:36:26 +0100

    une mise a jour

--- backend/contrat_chauffeur/models.py
+++ backend/contrat_chauffeur/models.py
@@ -83,8 +83,8 @@ class ContratChauffeur(TimeStampedModel):
 
     date_signature = models.DateField(null=True, blank=True)
     date_debut = models.DateField(null=True, blank=True)
-    date_concernee = models.DateTimeField()
-    date_limite = models.DateTimeField()
+    date_concernee = models.DateField()
+    date_limite = models.DateField()
 
     # Use days as integer
     duree_jour = models.PositiveIntegerField(null=True, blank=True, help_text=_("Durée du contrat en jours"))
