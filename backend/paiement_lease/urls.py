a6ca0c57f266d4a1ce2d889c590c2f7ea5f93d4c
Proxym <it_management@proxymgroup.com>
2025-10-14 12:09:59 +0100

    API Calendrier de paiements des chauffeurs

--- backend/paiement_lease/urls.py
+++ backend/paiement_lease/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from paiement_lease.views import  PaiementLeaseAPIView, \
     LeaseCombinedListAPIView, LeaseCombinedExportXLSX, LeaseCombinedExportCSV
+from .views import calendrier_paiements_contrat
 
 urlpatterns = [
     path("lease/pay", PaiementLeaseAPIView.as_view(), name="lease-pay"),
     path("lease/combined", LeaseCombinedListAPIView.as_view(), name="lease-combined"),
     path("lease/combined/export/xlsx", LeaseCombinedExportXLSX.as_view(), name="lease-combined-export-excel"),
-    path("lease/combined/export/csv", LeaseCombinedExportCSV.as_view(), name="lease-combined-export-csv")
+    path("lease/combined/export/csv", LeaseCombinedExportCSV.as_view(), name="lease-combined-export-csv"),
+    path("paiements/calendrier/<int:contrat_id>/", calendrier_paiements_contrat, name="calendrier-paiements-contrat"),
 
 ]
