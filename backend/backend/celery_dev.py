e738311ccc937745dfb646ae23be872b93b52a48
MBY EKONDE Christian Durand <christiandior512@gmail.com>
2025-10-16 12:49:05 +0100

    amelioration des filtres dans la tableau des filtres

--- backend/backend/celery_dev.py
+++
@@ -1,8 +0,0 @@
-# backend/celery_dev.py
-import os
-
-# Force Django à charger les settings DEV
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.dev")
-
-# Réutilise l'app Celery déclarée dans backend/celery.py
-from .celery import app  # noqa: F401
--- backend/backend/celery_prod.py
+++
@@ -1,7 +0,0 @@
-# backend/celery_prod.py
-import os
-
-# Force Django à charger les settings PROD
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.prod")
-
-from .celery import app  # noqa: F401
